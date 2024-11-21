67144396dbc43e9fb81a098d67c7772305aef9cf
Jathu23 <jathushanj2003@gmail.com>
2024-11-21 19:45:01 +0530

    search function audiobook

--- library management system frontend/src/app/services/bookservice/getbooks.service.ts
+++ library management system frontend/src/app/services/bookservice/getbooks.service.ts
@@ -10,9 +10,14 @@ export class GetbooksService {
   constructor(private http: HttpClient) {}
 
 private audiobookUrl = `https://localhost:7261/api/Audiobook/GetAudiobooks`;
+private audiobookUrld = `https://localhost:7261/api/Audiobook/`;
 
 getaudiobooks(currentPage:number,pageSize:number): Observable<any> {
-  return this.http.get<any>(this.audiobookUrl + `?page=${currentPage}&pageSize=${pageSize}`);
+  return this.http.get<any>(this.audiobookUrld + `GetAudiobooks?page=${currentPage}&pageSize=${pageSize}`);
+}
+
+searchAudiobooks(searchString: string,currentPage: number,pageSize: number): Observable<any> {
+  return this.http.get<any>( this.audiobookUrld +`Search?searchString=${searchString}&page=${currentPage}&pageSize=${pageSize}`);
 }
 
 
