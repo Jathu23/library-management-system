aa71279dd962a920b4ae5ad9a6c931a807bac9a7
Jathu23 <jathushanj2003@gmail.com>
2024-11-22 12:29:35 +0530

    update get normalbook api

--- library management system frontend/src/app/app.module.ts
+++ library management system frontend/src/app/app.module.ts
@@ -5,19 +5,13 @@ import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AddbookComponent } from './components/admin/addbook/addbook.component';
-import { AdminDashboardComponent } from './components/admin/admin-dashboard/admin-dashboard.component';
-import { ManageBooksComponent } from './components/admin/manage-books/manage-books.component';
-import { ManageUsersComponent } from './components/admin/manage-users/manage-users.component';
 import { UserDashboardComponent } from './components/user/user-dashboard/user-dashboard.component';
 import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
 import { LandingLayoutComponent } from './layouts/landing-layout/landing-layout.component';
 import { UserLayoutComponent } from './layouts/user-layout/user-layout.component';
 import { AboutusComponent } from './layouts/landing-layout/aboutus/aboutus.component';
-import { AdmincreateComponent } from './components/landing/admincreate/admincreate.component';
 import { UsersignupComponent } from './components/landing/usersignup/usersignup.component';
 import { LoginComponent } from './components/landing/login/login.component';
-import { AudiobooksComponent } from './components/admin/audiobooks/audiobooks.component';
-import { AudiobookDetailsComponent } from './components/admin/audiobook-details/audiobook-details.component';
 import { AddebookComponent } from './components/admin/addebook/addebook.component';
 import { AddAudiobookComponent } from './components/admin/add-audiobook/add-audiobook.component';
 import { ShowbooksComponent } from './components/user/showbooks/showbooks.component';
@@ -37,19 +31,15 @@ import { CommonModule } from '@angular/common';
   declarations: [
     AppComponent,
     AddbookComponent,
-    AdminDashboardComponent,
-    ManageBooksComponent,
-    ManageUsersComponent,
     UserDashboardComponent,
     AboutusComponent,
     AdminLayoutComponent,
     LandingLayoutComponent,
     UserLayoutComponent,
-    AdmincreateComponent,
+    AdminLayoutComponent,
     UsersignupComponent,
     LoginComponent,
-    AudiobooksComponent,
-    AudiobookDetailsComponent,
+    AddAudiobookComponent,
     AddebookComponent,
     AddAudiobookComponent,
     ShowbooksComponent,
