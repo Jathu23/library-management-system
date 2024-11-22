125e40900faff8c31139d130b9b5542b1c493ac0
Jathu23 <jathushanj2003@gmail.com>
2024-11-22 11:36:09 +0530

    test

--- library management system frontend/src/app/app.module.ts
+++ library management system frontend/src/app/app.module.ts
@@ -3,19 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
-
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AddbookComponent } from './components/admin/addbook/addbook.component';
 import { AdminDashboardComponent } from './components/admin/admin-dashboard/admin-dashboard.component';
 import { ManageBooksComponent } from './components/admin/manage-books/manage-books.component';
 import { ManageUsersComponent } from './components/admin/manage-users/manage-users.component';
-
 import { UserDashboardComponent } from './components/user/user-dashboard/user-dashboard.component';
 import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
 import { LandingLayoutComponent } from './layouts/landing-layout/landing-layout.component';
 import { UserLayoutComponent } from './layouts/user-layout/user-layout.component';
 import { AboutusComponent } from './layouts/landing-layout/aboutus/aboutus.component';
-
 import { AdmincreateComponent } from './components/landing/admincreate/admincreate.component';
 import { UsersignupComponent } from './components/landing/usersignup/usersignup.component';
 import { LoginComponent } from './components/landing/login/login.component';
@@ -23,13 +20,12 @@ import { AudiobooksComponent } from './components/admin/audiobooks/audiobooks.co
 import { AudiobookDetailsComponent } from './components/admin/audiobook-details/audiobook-details.component';
 import { AddebookComponent } from './components/admin/addebook/addebook.component';
 import { AddAudiobookComponent } from './components/admin/add-audiobook/add-audiobook.component';
-
 import { ShowbooksComponent } from './components/user/showbooks/showbooks.component';
 import { ShowaudiobooksComponent } from './components/user/showaudiobooks/showaudiobooks.component';
-
 import { NewAdminComponent } from './components/admin/new-admin/new-admin.component';
 import { ShowebooksComponent } from './components/user/showebooks/showebooks.component';
 import { ShowNormalbookComponent } from './components/admin/show-normalbook/show-normalbook.component';
+import { CommonModule } from '@angular/common';
 
 
 
@@ -58,16 +54,10 @@ import { ShowNormalbookComponent } from './components/admin/show-normalbook/show
     AddAudiobookComponent,
     ShowbooksComponent,
     ShowaudiobooksComponent,
+   NewAdminComponent,
+   ShowNormalbookComponent,
+   ShowebooksComponent
 
-    NewAdminComponent,
-<<<<<<< HEAD
-    NewAdminComponent,
-    ShowebooksComponent,
-=======
-      ShowNormalbookComponent
-
-   
->>>>>>> 438fccb7bc21e538bbce8de9d1cbe0e66fd5522a
   ],
 
   imports: [
@@ -76,6 +66,9 @@ import { ShowNormalbookComponent } from './components/admin/show-normalbook/show
     HttpClientModule,
     FormsModule,
     ReactiveFormsModule,
+    CommonModule,
+   
+
      
   ],
   providers: [],
