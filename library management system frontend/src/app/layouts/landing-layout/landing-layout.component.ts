74f8f84e3ff2e97086c9408548b89303a38341a4
Jathu23 <jathushanj2003@gmail.com>
2024-11-29 18:28:01 +0530

    Authorization  ok

--- library management system frontend/src/app/layouts/landing-layout/landing-layout.component.ts
+++ library management system frontend/src/app/layouts/landing-layout/landing-layout.component.ts
@@ -14,6 +14,8 @@ export class LandingLayoutComponent  {
   isLoading = false;
   signupForm: FormGroup;
   loginForm!: FormGroup;
+isLoading2: boolean = false;
+isLoading3:  boolean = false;
 
   constructor(
     private fb: FormBuilder,
@@ -54,18 +56,45 @@ export class LandingLayoutComponent  {
   
   onSubmit() {
     if (this.signupForm.valid) {
+      this.isLoading2=true;
       console.log('Form Submitted', this.signupForm.value);
       const formData = this.signupForm.value;
       this.authService.createuser(formData).subscribe(
         (response) =>{
           if (response.success) {
+
+            setTimeout(() => {
+              this.isLoading2=false;
+              this.isLoading3=true;
+            }, 1000);
+            localStorage.setItem('token',response.data.token);
+              setTimeout(() => {
+                if (response.data.role == "admin") {
+                  this.isLoading3=false;
+                  this.router.navigate(['/admin']); 
+                }else{
+                  this.isLoading3=false;
+                  this.router.navigate(['/user']); 
+                }
+              }, 3300);
+          
             console.log('User create successfully:', response.data);
           } else {
+            this.isLoading2=false;
             console.error('User create : ', response.errors);
             
           }
         },
         (error) =>{
+          setTimeout(
+            ()=>{
+              this.isLoading2 = false;
+             
+            },500
+           );
+           setTimeout(() => {
+            alert(error.error.errors[0]);
+          }, 520);
           console.error('An error occurred:', error);
         }
       );
@@ -73,9 +102,9 @@ export class LandingLayoutComponent  {
   }
 
   onLogin() {
-    console.log("loding 1 :" ,this.isLoading);
+   
     this.isLoading = true;
-    console.log("loding 2 :" ,this.isLoading);
+   
 
     if (this.loginForm.valid) {
       const formData = this.loginForm.value;
@@ -84,26 +113,42 @@ export class LandingLayoutComponent  {
       this.authService.login(formData).subscribe(
         (response) => {
           if (response.success) {
-            this.isLoading = false;
-            console.log('Admin logged in successfully:', response.data);
-            console.log("loding sucess 3 :" ,this.isLoading);
-              if (response.data.role == "admin") {
-                this.router.navigate(['/admin']); 
-              }else{
-                this.router.navigate(['/user']); 
-              }
+            setTimeout(() => {
+              this.isLoading=false;
+              this.isLoading3=true;
+            }, 1000);
+            localStorage.setItem('token',response.data.token);
+              setTimeout(() => {
+                if (response.data.role == "admin") {
+                  this.isLoading3=false;
+                  this.router.navigate(['/admin']); 
+                }else{
+                  this.isLoading3=false;
+                  this.router.navigate(['/user']); 
+                }
+              }, 3300);
             console.log( response.data.role); 
             
           } else {
-            this.isLoading = false;
+            
+              this.isLoading = false;
+          
             console.error('Login failed:', response.errors);
          
           }
         },
         (error) => {
-          this.isLoading = false;
+          setTimeout(
+            ()=>{
+              this.isLoading = false;
+             
+            },300
+           );
+           setTimeout(() => {
+            alert(error.error.errors[0]);
+          }, 320);
           console.error('An error occurred:', error);
-          console.log("loding  error 4:" ,this.isLoading);
+         
         }
       );
 
