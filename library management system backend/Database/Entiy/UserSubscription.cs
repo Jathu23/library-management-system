1ac7404823d428673bf1a70578a041180bac8b0f
Jathu23 <jathushanj2003@gmail.com>
2024-12-01 12:54:21 +0530

    Subscription tables add

---
+++ library management system backend/Database/Entiy/UserSubscription.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace library_management_system.Database.Entiy
+{
+    public class UserSubscription
+    {
+        [Key]
+        public int Id { get; set; }                 // Primary key
+        public int UserId { get; set; }            // Foreign key to User
+        public int SubscriptionPlanId { get; set; } // Foreign key to SubscriptionPlan
+        public DateTime StartDate { get; set; }    // Subscription start date
+        public DateTime EndDate { get; set; }      // Subscription end date
+        public string Status { get; set; }         // Active, Expired, Cancelled, etc.
+        public DateTime CreatedDate { get; set; } // When the record was created
+        public DateTime UpdatedDate { get; set; } // When the record was last updated
+
+
+        // Navigation properties
+        public User User { get; set; }
+        public SubscriptionPlan SubscriptionPlan { get; set; }
+    }
+
+}
--- library management system backend/Migrations/20241201071728_Subscription.Designer.cs
+++ library management system backend/Migrations/20241201071728_Subscription.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using library_management_system.Database;
 
@@ -11,9 +12,11 @@ using library_management_system.Database;
 namespace library_management_system.Migrations
 {
     [DbContext(typeof(LibraryDbContext))]
-    partial class LibraryDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241201071728_Subscription")]
+    partial class Subscription
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
